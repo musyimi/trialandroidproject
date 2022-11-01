97ba4d51d68d34adae41be4df04a4c018800be43
ngusu <musyimi@users.noreply.github.com>
2022-11-01 14:56:35 +0300

    adding neccessary packages in app/java/com.example.trial package

--- app/src/main/java/com/example/trial/MainActivity.kt
+++ app/src/main/java/com/example/trial/MainActivity.kt
@@ -11,17 +11,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.trial.ui.theme.TrialTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TrialTheme {
+
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
                     Greeting("Android")
                 }
             }
---
+++ app/src/main/java/com/example/trial/TrialApplication.kt
@@ -0,0 +1,8 @@
+package com.example.trial
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TrialApplication: Application() {
+}
