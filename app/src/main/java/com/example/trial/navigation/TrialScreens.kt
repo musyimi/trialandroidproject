3f7e0853bd04c1630ab8f19735576b93f3c6c79e
ngusu <musyimi@users.noreply.github.com>
2022-11-01 15:34:34 +0300

    adding routing

--- app/src/main/java/com/example/trial/navigation/TrialScreens.kt
+++ app/src/main/java/com/example/trial/navigation/TrialScreens.kt
@@ -1,4 +1,24 @@
 package com.example.trial.navigation
 
 enum class TrialScreens {
+    SplashScreen,
+    LoginScreen,
+    HomeScreen,
+    DetailsScreen,
+    SearchScreen,
+    UpdateScreen,
+
+    companion object {
+        fun fromRoute(route: String?): TrialScreens =
+            when(route?.substringBefore("/")) {
+                SplashScreen.name -> SplashScreen
+                LoginScreen.name -> LoginScreen
+                HomeScreen.name -> HomeScreen
+                DetailsScreen.name -> DetailsScreen
+                SearchScreen.name -> SearchScreen
+                UpdateScreen.name -> UpdateScreen
+                null -> HomeScreen
+                else -> throw IllegalArgumentException("Route $route is not recognised")
+            }
+    }
 }
